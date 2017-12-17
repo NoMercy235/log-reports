bcd66a892c308e3226f0cf72fc8fc1c73835a2b6
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2017-12-17 13:22:39 +0200

    Implemented the file-manager functionality. It gets the content of all files changed in the WATCH folder and places them into a single file placed in the RESULT path.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+const Subject = require('rxjs/Subject').Subject;
+require('rxjs/add/observable/of');
+require('rxjs/add/operator/map');
+require('rxjs/add/operator/debounceTime');
+
 const watch = require('node-watch');
 const emailSender = require('./lib/email-sender');
+const fileManager = require('./lib/file-manager');
+const env = require('./env');
 
+const subject = new Subject();
 
-watch('./test', { recursive: true }, (event, name) => {
+watch(env.watchPath, { recursive: true }, (event, name) => {
     console.log(event);
     console.log(name);
-    emailSender({});
+    if (event === 'update') {
+        fileManager.readFile(name).then(() => {
+            subject.next();
+        });
+    }
+});
+
+subject.debounceTime(1000).subscribe(() => {
+    console.log('Should check result');
+    if (env.mail.sendEmail) {
+        emailSender({});
+    }
+    fileManager.emptyFile('./result/result.txt');
 });
